672cdd553a9d2b091b27a74a851c5cfaa3a4bdf2
rytmur <rytmur41@gmail.com>
2024-08-12 14:37:50 +0900

    fix: 外部キーにNOT NULL制約を適用

--- src/main/resources/db/migration/V1.0.1__books.sql
+++ src/main/resources/db/migration/V1.0.1__books.sql
@@ -1,7 +1,7 @@
 CREATE TABLE books (
     book_id    SERIAL PRIMARY KEY,
     title      VARCHAR(255) NOT NULL,
-    author_id  INT REFERENCES authors (author_id),
+    author_id  INT REFERENCES authors (author_id) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
